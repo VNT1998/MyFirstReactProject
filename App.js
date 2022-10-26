84633a94d2b7f5e0b4bb271b9e5ae2d4f5db4bd1
V---N---T <vinit.k@aveosoft.com>
2022-10-26 20:41:53 +0530

    flex example

--- App.js
+++ App.js
@@ -7,14 +7,7 @@
  */
 
 import React, {useState} from 'react';
-import {
-  Button,
-  Linking,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 var count = 0;
@@ -41,18 +34,35 @@ const App = () => {
   return (
     // <SafeAreaView>
     <View style={styles.body}>
-      <Text style={styles.text}>Hello this is {name}</Text>
-      <Text style={styles.text}>Button has been clicked {session.number}</Text>
-      <Text style={styles.text}>
-        {current ? 'Current session' : 'Next session'}
-      </Text>
-      <Button title="Update Name" onPress={onClickHandler} />
-      <Button
-        title="Demo Button"
-        onPress={() => {
-          Linking.openURL('https://icons8.com/');
-        }}
-      />
+      <View style={styles.row1}>
+        <View style={styles.text1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.text2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.text3}>
+          <Text style={styles.text}>3</Text>
+        </View>
+      </View>
+      <View style={styles.row2}>
+        <View style={styles.text4}>
+          <Text style={styles.text}>4</Text>
+        </View>
+      </View>
+      <View style={styles.row3}>
+        <View style={styles.text5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+      <View style={styles.row4}>
+        <View style={styles.text6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.text7}>
+          <Text style={styles.text}>7</Text>
+        </View>
+      </View>
     </View>
     // </SafeAreaView>
   );
@@ -60,22 +70,83 @@ const App = () => {
 
 const styles = StyleSheet.create({
   body: {
-    // flex: 1,
-    height: '50%',
-    width: '50%',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  row1: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  row2: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  row3: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  row4: {
+    flex: 5,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+
+  text1: {
+    flex: 1,
+    backgroundColor: '#0AF7F4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text2: {
+    flex: 2,
+    backgroundColor: '#F70AE9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text3: {
+    flex: 3,
+    backgroundColor: '#F7F70A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text4: {
+    flex: 1,
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text5: {
+    flex: 1,
+    backgroundColor: '#428E36',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text6: {
+    flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 15,
-    borderColor: '#ff0000',
-    borderWidth: 20,
-    margin: 49,
+  },
+  text7: {
+    flex: 1,
+    backgroundColor: '#364E8E',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
-    color: '#ff0000',
+    color: '#000000',
     fontSize: 20,
     fontStyle: 'italic',
-    margin: 20,
   },
 });
 

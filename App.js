b42c169d306ddf9887878b6831c88bd7f29da6fb
Vinit Karkera <42674250+VNT1998@users.noreply.github.com>
2022-11-06 00:43:10 +0530

    SectionList

--- App.js
+++ App.js
@@ -7,19 +7,10 @@
  */
 
 import React, {useState} from 'react';
-import {
-  FlatList,
-  RefreshControl,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import SectionList from 'react-native/Libraries/Lists/SectionList';
 
-var count = 0;
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const [Items, setItems] = useState([
     {name: 'Item 1'},
     {name: 'Item 2'},
@@ -43,26 +34,53 @@ const App = () => {
     setItems([...Items, {name: 'Item 15'}]);
     setRefreshing(false);
   };
+
+  const sectionData = [
+    {title: 'Title 1', data: ['Item 1-1', 'Item 1-2', 'Item 1-3']},
+    {title: 'Title 2', data: ['Item 2-1', 'Item 2-2', 'Item 2-3']},
+    {title: 'Title 3', data: ['Item 3-1']},
+    {title: 'Title 4', data: ['Item 4-1', 'Item 4-2', 'Item 4-3']},
+    {title: 'Title 5', data: ['Item 5-1']},
+    {title: 'Title 6', data: ['Item 6-1', 'Item 6-2']},
+    {title: 'Title 7', data: ['Item 7-1', 'Item 7-2', 'Item 7-3']},
+    {title: 'Title 8', data: ['Item 8-1', 'Item 8-2']},
+  ];
+
   return (
-    <View style={styles.body}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item, index) => index.toString()}
-        data={Items}
-        renderItem={({item}) => (
-          <View style={styles.item}>
-            <Text style={styles.text}>{item.name}</Text>
-          </View>
-        )}
-        refreshControl={
-          <RefreshControl
-            refreshing={Refreshing}
-            onRefresh={onRefresh}
-            colors={['#ff00ff']}
-          />
-        }
-      />
-    </View>
+    <SectionList
+      sections={sectionData}
+      style={styles.body}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({section}) => (
+        <View style={styles.itemh}>
+          <Text style={styles.text}>{section.title}</Text>
+        </View>
+      )}
+    />
+    // <View style={styles.body}>
+    //   <FlatList
+    //     numColumns={2}
+    //     keyExtractor={(item, index) => index.toString()}
+    //     data={Items}
+    //     renderItem={({item}) => (
+    //       <View style={styles.item}>
+    //         <Text style={styles.text}>{item.name}</Text>
+    //       </View>
+    //     )}
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={Refreshing}
+    //         onRefresh={onRefresh}
+    //         colors={['#ff00ff']}
+    //       />
+    //     }
+    //   />
+    // </View>
 
     // <SafeAreasssView>
     // <View style={styles.body}>
@@ -99,6 +117,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#4ae1fa',
+  },
+  itemh: {
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff00ff',
   },
 
   text: {

37b38bb1c3474d1c61d65155153d2638c5c4e162
Vinit Karkera <42674250+VNT1998@users.noreply.github.com>
2022-11-06 00:22:19 +0530

    Flatlist

--- App.js
+++ App.js
@@ -8,8 +8,8 @@
 
 import React, {useState} from 'react';
 import {
+  FlatList,
   RefreshControl,
-  ScrollView,
   StyleSheet,
   Text,
   useColorScheme,
@@ -21,49 +21,69 @@ const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const [Items, setItems] = useState([
-    {key: 1, item: 'Item 1'},
-    {key: 2, item: 'Item 2'},
-    {key: 3, item: 'Item 3'},
-    {key: 4, item: 'Item 4'},
-    {key: 5, item: 'Item 5'},
-    {key: 6, item: 'Item 6'},
-    {key: 7, item: 'Item 7'},
-    {key: 8, item: 'Item 8'},
-    {key: 9, item: 'Item 9'},
-    {key: 10, item: 'Item 10'},
-    {key: 11, item: 'Item 11'},
-    {key: 12, item: 'Item 12'},
-    {key: 13, item: 'Item 13'},
-    {key: 14, item: 'Item 14'},
+    {name: 'Item 1'},
+    {name: 'Item 2'},
+    {name: 'Item 3'},
+    {name: 'Item 4'},
+    {name: 'Item 5'},
+    {name: 'Item 6'},
+    {name: 'Item 7'},
+    {name: 'Item 8'},
+    {name: 'Item 9'},
+    {name: 'Item 10'},
+    {name: 'Item 11'},
+    {name: 'Item 12'},
+    {name: 'Item 13'},
+    {name: 'Item 14'},
   ]);
   const [Refreshing, setRefreshing] = useState(false);
 
   const onRefresh = () => {
     setRefreshing(true);
-    setItems([...Items, {key: 15, item: 'Item 15'}]);
+    setItems([...Items, {name: 'Item 15'}]);
     setRefreshing(false);
   };
   return (
-    // <SafeAreasssView>
     <View style={styles.body}>
-      <ScrollView
-        horizontal
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item, index) => index.toString()}
+        data={Items}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{item.name}</Text>
+          </View>
+        )}
         refreshControl={
           <RefreshControl
             refreshing={Refreshing}
             onRefresh={onRefresh}
             colors={['#ff00ff']}
           />
-        }>
-        {Items.map(i => {
-          return (
-            <View style={styles.item} key={i.key}>
-              <Text style={styles.text}>{i.item}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+        }
+      />
     </View>
+
+    // <SafeAreasssView>
+    // <View style={styles.body}>
+    //   <ScrollView
+    //     horizontal
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={Refreshing}
+    //         onRefresh={onRefresh}
+    //         colors={['#ff00ff']}
+    //       />
+    //     }>
+    //     {Items.map(i => {
+    //       return (
+    //         <View style={styles.item} key={i.key}>
+    //           <Text style={styles.text}>{i.item}</Text>
+    //         </View>
+    //       );
+    //     })}
+    //   </ScrollView>
+    // </View>
     /* </SafeAreaView> */
   );
 };
